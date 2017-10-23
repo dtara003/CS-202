4c5ff56ae19da967bac4b9410d32db8b126b3f8a
Dharti Tarapara <dtara003@ucr.edu>
2017-10-23 02:48:00 +0000

    1.1

--- Project_2/hw2.cpp
+++ Project_2/hw2.cpp
@@ -3,16 +3,16 @@
 #include "blas.h"
 using namespace std;
 
+double randVal() {
+    double div = RAND_MAX / 9.0;
+    return (rand() / div);
+}
+
 int main()
 {
-    char    TRANS = 'N';
-    int     INFO = 3;
-    int     LDA = 3;
-    int     LDB = 3;
-    int     N = 3;
-    int     NRHS = 1;
-    int     IPIV[3];
-    // use new to allocate memory if you need large space
+    srand(time(NULL));
+    
+    /*// use new to allocate memory if you need large space
     // Here, we want to solve AX = b
     //    x1 + 2x2 + 3x3 = 1
     //    2x1 + x2 + x3  = 1
@@ -28,8 +28,9 @@ int main()
     //  A' = { 1 2 1
     //         2 1 1
     //         3 1 1 }
-    // correct solution = {0 2 -1}'
-    double  A[9] =
+    // correct solution = {0 2 -1}'*/
+    
+    /*double  A[9] =
 	{
 	    1, 2, 1,
 	    2, 1, 1,
@@ -41,41 +42,70 @@ int main()
 	    1,
 	    1,
 	    1
-	};
-    // LU factorization
-    LAPACK_dgetrf(&N,&N,A,&LDA,IPIV,&INFO);
-
-    char     SIDE = 'L';
-    char     UPLO = 'L';
-    char     DIAG = 'U';
-    int      M    = 1;
-    double   a    = 1.0;
-    // This function solve the Ax=B directly
-    //dgetrs_(&TRANS,&N,&NRHS,A,&LDA,IPIV,B,&LDB,&INFO);
-
-    // change the order of B according to IPIV[] from LU factorization
-
-    for(int i = 0; i < N; i++)
-    {
-        double tmp = B[IPIV[i]-1];
-	B[IPIV[i]-1] = B[i];
-	B[i] = tmp;
-    }
-
-    // forward  L(Ux) = B => y = Ux
-    dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A, &N, B, &N);
-    UPLO = 'U';
-    DIAG = 'N';
-    // backward Ux = y
-    dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A, &N, B, &N);
+	};*/
+	
+	int nSize[5] = {2, 1000, 2000, 3000, 4000, 5000};
+	
+	int x;
+    for (x = 0; x < 6; x++) {
+        int n = nSize[x];
+        char    TRANS = 'N';
+        int     INFO = n;
+        int     LDA = n;
+        int     LDB = n;
+        int     N = n;
+        int     NRHS = 1;
+        int     IPIV[n];
+        
+        cout << "n = " << n << endl;
+        double* A = (double*)malloc(n * n * sizeof(double));
+        double* B = (double*)malloc(n * sizeof(double));
+        
+        // generate random matrices
+        for (i = 0; i < (n * n); i++) {
+            if (i < n) {
+                A[i] = randVal();
+                B[i] = randVal();
+            } else {
+                A[i] = randVal();
+            }
+        }
+        
+        // LU factorization
+        LAPACK_dgetrf(&N,&N,A,&LDA,IPIV,&INFO);
     
-    cout << "print the result : {";
-    int i;
-    for (i=0;i<N;i++)
-    {
-	cout << B[i] << " ";
+        char     SIDE = 'L';
+        char     UPLO = 'L';
+        char     DIAG = 'U';
+        int      M    = 1;
+        double   a    = 1.0;
+        // This function solve the Ax=B directly
+        //dgetrs_(&TRANS,&N,&NRHS,A,&LDA,IPIV,B,&LDB,&INFO);
+    
+        // change the order of B according to IPIV[] from LU factorization
+    
+        for(int i = 0; i < N; i++)
+        {
+            double tmp = B[IPIV[i]-1];
+    	B[IPIV[i]-1] = B[i];
+    	B[i] = tmp;
+        }
+    
+        // forward  L(Ux) = B => y = Ux
+        dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A, &N, B, &N);
+        UPLO = 'U';
+        DIAG = 'N';
+        // backward Ux = y
+        dtrsm_(&SIDE,&UPLO,&TRANS,&DIAG,&N,&M,&a,A, &N, B, &N);
+        
+        cout << "print the result : {";
+        int i;
+        for (i=0;i<N;i++)
+        {
+    	cout << B[i] << " ";
+        }
+        cout << "}" << endl;
     }
-    cout << "}" << endl;
     
     return 0;
 }
