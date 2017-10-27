8d5e3e17883448777110bf525fd01fe16e3cc836
Dharti Tarapara <dtara003@ucr.edu>
2017-10-27 05:43:53 +0000

    LU decomposition, forward substitution, backward substitution

--- Project_2/hw2.cpp
+++ Project_2/hw2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-#include "lapacke.h"
-#include "blas.h"
+// #include "lapacke.h"
+// #include "blas.h"
+#include <cmath>
+#include <cstdlib>
+
 using namespace std;
 
 double randVal() {
@@ -8,15 +11,101 @@ double randVal() {
     return (rand() / div);
 }
 
-void mydgetrf(double* A, double* B) {
-    int n = 2;
-    for (int i = 0; i < n; i++) {
+void mydgetrf(double* A, double* B, int* pvt, int n) {
+    // check contents
+    /*for (int i = 0; i < n; i++) {
+    		for (int j = 0; j < n; j++) {
+    			cout << A[i * n + j] << " ";
+    		}
+    		cout << endl;
+    	}
+    cout << endl;*/
+    
+    for (int i = 0; i < n - 1; i++) {
+        // pivot
+        int maxind = i;
+        double max = abs(A[i * n + i]);
+        
+        for (int t = i + 1; t < n; t++) {
+            if (abs(A[t * n + i]) > max) {
+                maxind = t;
+                max = abs(A[t * n + i]);
+            }
+        }
+        
+        if (max == 0.0) {
+            cout << "LU factorization failed: coefficient matrix is singular" << endl;
+            return;
+        } else {
+            if (maxind != i) {
+                // save pivoting information
+                int temps = pvt[i];
+                pvt[i] = pvt[maxind];
+                pvt[maxind] = temps;
+                
+                double* tempv = (double*)malloc(n * sizeof(double));
+                tempv[0] = 0.0;
+                
+                // swap rows
+                for (int j = 0; j < n; j++) {
+                    tempv[j] = A[i * n + j];
+                    A[i * n + j] = A[maxind * n + j];
+                    A[maxind * n + j] = tempv[j];
+                }
+                
+                free(tempv);
+            }
+        }
+        
+        // factorization
+        for (int j = i + 1; j < n; j++) {
+            A[j * n + i] = A[j * n + i] / A[i * n + i];
+            
+            for (int k = i + 1; k < n; k++) {
+                A[j * n + k] = A[j * n + k] - A[j * n + i] * A[i * n + k];
+            }
+        }
+    }
+    
+    // check contents
+    /*for (int i = 0; i < n; i++) {
     		for (int j = 0; j < n; j++) {
     			cout << A[i * n + j] << " ";
     		}
     		cout << endl;
     	}
     cout << endl;
+    for (int i = 0; i < n; i++) {
+        cout << pvt[i] << endl;
+    }*/
+    
+    return;
+}
+
+void mydtrsm1(double* A, double* B, int* pvt, double* y, int n) {
+    y[0] = B[pvt[0]];
+    
+    for (int i = 1; i < n; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < i - 1; j++) {
+            sum += y[j] * A[i * n +j];
+        }
+        y[i] = B[pvt[i]] - sum;
+    }
+    
+    return;
+}
+
+void mydtrsm2(double* A, double* x, double* y, int n) {
+    x[n - 1] = y[n- 1] / A[n * n + n];
+    
+    for (int i = n - 2; i >= 0; i--) {
+        double sum = 0.0;
+        for (int j = i + 1; j < n; j++) {
+            sum += x[j] * A[i * n + j];
+        }
+        x[i] = (y[i] - sum) / A[i * n + i];
+    }
     
     return;
 }
@@ -39,7 +128,7 @@ int main()
 	    1
 	};*/
 	
-	int nSize[6] = {2, 1000, 2000, 3000, 4000, 5000};
+	int nSize[6] = {3, 1000, 2000, 3000, 4000, 5000};
 	
 	int x;
     for (x = 0; x < 1; x++) {
@@ -57,8 +146,12 @@ int main()
         // allocate arrays
         double* A = (double*)malloc(n * n * sizeof(double));
         double* B = (double*)malloc(n * sizeof(double));
+        
         double* A2 = (double*)malloc(n * n * sizeof(double));
         double* B2 = (double*)malloc(n * sizeof(double));
+        int* pvt = (int*)malloc(n * sizeof(int));
+        double* x = (double*)malloc(n * sizeof(double));
+        double* y = (double*)malloc(n * sizeof(double));
         
         // generate random matrices
         for (int i = 0; i < (n * n); i++) {
@@ -67,11 +160,15 @@ int main()
                 B[i] = randVal();
                 A2[i] = A[i];
                 B2[i] = B[i];
+                pvt[i] = i;
             } else {
                 A[i] = randVal();
                 A2[i] = A[i];
             }
         }
+        
+        x[0] = 0.0;
+        y[0] = 0.0;
         
         // output for debugging
     	for (int i = 0; i < n; i++) {
@@ -81,10 +178,10 @@ int main()
     		cout << endl;
     	}
     	cout << endl;
-    
     	for (int i = 0; i < n; i++) {
     		cout << B[i] << endl;
     	}
+    	cout << endl;
 	
         // LU factorization
         LAPACK_dgetrf(&N,&N,A,&LDA,IPIV,&INFO);
@@ -121,11 +218,22 @@ int main()
         }
         cout << "}" << endl;
         
-	// cout << "GOT HERE" << endl;
-        mydgetrf(A2, B2);
-        // cout << "AND HERE" << endl;
+        // LU decomposition
+        mydgetrf(A2, B2, pvt, n);
         
-	free(A); free(B); free(A2); free(B2);
+        // forward substitution
+        mydtrsm1(A2, B2, pvt, y, n);
+        // backward substitution
+        mydtrsm2(A2, x, y, n);
+        
+        cout << endl;
+        for (int i = 0; i < n; i++) {
+            cout << x[i] << endl;
+        }
+        
+        
+        
+	    free(A); free(B); free(A2); free(B2); free(pvt); free(x); free(y);
     }
     
     return 0;
