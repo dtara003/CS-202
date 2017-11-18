7c8bd474405f912b5359b09345f1b8bee32977f8
Dharti Tarapara <dtara003@ucr.edu>
2017-11-17 16:18:29 -0800

    part 1 done

--- Project_3/part1.c
+++ Project_3/part1.c
@@ -31,11 +31,11 @@ int main (int argc, char *argv[])
           MPI_Finalize(); exit (1);
     }
    n = atoll(argv[1]);
-   low_value = 2 + BLOCK_LOW(id,p,n-1);
-   high_value = 2 + BLOCK_HIGH(id,p,n-1);
-   size = BLOCK_SIZE(id,p,n-1);
-   proc0_size = (n-1)/p;
-   if ((2 + proc0_size) < (int) sqrt((double) n)) {
+   low_value = 3 + BLOCK_LOW(id,p,n-2) + (BLOCK_LOW(id,p,n-2) % 2);
+   high_value = 3 + BLOCK_HIGH(id,p,n-2) - (BLOCK_HIGH(id,p,n-2) % 2);
+   size = (high_value - low_value) / 2 + 1;
+   proc0_size = (n-2)/(2*p);
+   if ((3 + proc0_size) < (int) sqrt((double) n)) {
       if (!id) printf ("Too many processes\n");
       MPI_Finalize();
       exit (1);
@@ -48,30 +48,36 @@ int main (int argc, char *argv[])
    }
    for (i = 0; i < size; i++) marked[i] = 0;
    if (!id) index = 0;
-   prime = 2;
+   prime = 3;
    do {
       if (prime * prime > low_value)
-         first = prime * prime - low_value;
+         first = (prime * prime - low_value) / 2;
       else {
          if (!(low_value % prime)) first = 0;
-         else first = prime - (low_value % prime);
+         else {
+		if (((low_value % prime) % 2) == 0) {
+			first = prime - (low_value % prime) / 2;
+		} else {
+			first = (prime - (low_value % prime)) / 2;
+		}
+	}
       }
       for (i = first; i < size; i += prime) marked[i] = 1;
       if (!id) {
          while (marked[++index]);
-         prime = index + 2;
+         prime = 2 * index + 3;
       }
-      MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
+      if (p > 1) MPI_Bcast (&prime,  1, MPI_INT, 0, MPI_COMM_WORLD);
    } while (prime * prime <= n);
    count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
-   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
+   if (p > 1) MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
       0, MPI_COMM_WORLD);
    elapsed_time += MPI_Wtime();
    if (!id) {
-      printf ("%llu primes are less than or equal to %llu\n",
-         global_count, n);
+	global_count++;
+      printf ("%llu primes are less than or equal to %llu\n", global_count, n);
       printf ("Total elapsed time: %10.6f\n", elapsed_time);
    }
    MPI_Finalize();
